333d464e3d67bbd790a594de7a32046fe53e8c01
quotawoosung <aaron@quotabook.com>
2020-07-26 18:54:38 +0900

    조직도 ordering순서 바꿈

--- minwoo_backend/information/models/model_people.py
+++ minwoo_backend/information/models/model_people.py
@@ -9,12 +9,12 @@ logger = logging.getLogger('logger')
 class People(models.Model) :
     name = models.CharField(max_length = 255, blank=False)
     position = models.CharField(max_length = 255, blank=False, help_text='직책')
-    ordering= models.IntegerField(help_text='화면에 보여질 순서(위에 보여질 수록 높은 숫자)', null=True, blank=True)
+    ordering= models.IntegerField(help_text='화면에 보여질 순서(낮은 숫자일수록 위에 보여짐)', null=True, blank=True)
     job = models.CharField(max_length = 255, blank=False, help_text='현재직업')
 
     class Meta:
         verbose_name_plural = '조직도 관리'
-        ordering = ['-ordering']
+        ordering = ['ordering']
 
     def __str__(self):
         return f'[pk={self.pk}, name={self.name}, position={self.position}, job={self.job}]'
