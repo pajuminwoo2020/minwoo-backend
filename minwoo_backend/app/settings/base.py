786c814ff1d205894d24ba88f37c098e91e8968d
NihiIist <cykim0315@gmail.com>
2020-08-25 22:11:23 +0900

    base.py 건드릴 부분 롤백

--- minwoo_backend/app/settings/base.py
+++ minwoo_backend/app/settings/base.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-
+    # Project-specific apps
     'hitcount',
     'user',
     'board',
