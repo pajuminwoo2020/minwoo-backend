eef4d846c063d05be39a86edc1b9fd33c3135486
quotawoosung <aaron@quotabook.com>
2020-09-02 02:31:10 +0900

    이미지 압축추가

--- minwoo_backend/board/models/model_image.py
+++ minwoo_backend/board/models/model_image.py
@@ -1,9 +1,14 @@
 import logging
+import sys
+from PIL import Image as PILImage
+from io import BytesIO
+import mimetypes
 
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from django.urls import reverse
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 logger = logging.getLogger('logger')
 
@@ -20,6 +25,27 @@ class Image(models.Model):
             return reverse('board:image', kwargs={'image_id': self.pk})
 
         return ''
+
+    def save(self, *args, **kwargs):
+        (mimetype, encoding) = mimetypes.guess_type(self.image_file.name)
+        if mimetype != 'image/jpeg':
+            # png같은 이미지 파일은 용량이 너무 커서 줄여줌
+            image_temporary = PILImage.open(self.image_file)
+            image_temporary = image_temporary.convert('RGB')
+            outputIoStream = BytesIO()
+            image_temporary.save(outputIoStream , format='JPEG', quality=60)
+            outputIoStream.seek(0)
+
+            self.image_file = InMemoryUploadedFile(
+                outputIoStream,
+                'ImageField',
+                "{}.jpg".format(self.image_file.name.split('.')[0]),
+                'image/jpeg',
+                sys.getsizeof(outputIoStream),
+                None,
+            )
+
+        super(Image, self).save(*args, **kwargs)
 
 
 @receiver(pre_delete, sender=Image)
--- minwoo_backend/information/models/model_society_about.py
+++ minwoo_backend/information/models/model_society_about.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('logger')
 
 class SocietyAbout(Image):
     name = models.CharField(max_length=255, blank=False, verbose_name='소모임명')
-    description = models.CharField(max_length=255, blank=True, verbose_name='소모임 설명')
+    description = models.TextField(blank=True, verbose_name='소모임 설명')
     schedule = models.TextField(blank=True, help_text="'-'로 줄을 구분하세요", verbose_name='소모임 정기 모임 날짜')
     main_activity = models.TextField(blank=True, help_text="'-'로 줄을 구분하세요", verbose_name='소모임 주요 활동')
     website = models.CharField(max_length=255, blank=True, verbose_name='소모임 홈페이지')
